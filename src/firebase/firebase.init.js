6c8ffeda4cd73446de9087dc617c65a8db56184c
farhanshahriyar <farhan.abir9999@gmail.com>
2023-09-28 19:31:35 +0600

    installed react router 6.16.0 and setup perfectly Firebase

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsweZ8ikFXYoArIHrLDLt6PYEIUOXmdbE",
+  authDomain: "simple-project-1-cf2c2.firebaseapp.com",
+  projectId: "simple-project-1-cf2c2",
+  storageBucket: "simple-project-1-cf2c2.appspot.com",
+  messagingSenderId: "856172493486",
+  appId: "1:856172493486:web:5ea8763b85790b56d8cb3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
