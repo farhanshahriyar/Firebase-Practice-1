60f14c8e417a07cc7b91b7e98b4e110a5719a3bb
farhanshahriyar <farhan.abir9999@gmail.com>
2023-09-29 23:22:10 +0600

    some condition on login and logout

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -31,17 +31,23 @@ const Login = () => {
 
 return (
 <div>
-    
-            <button onClick={handleGoogleSignIn}
-                className="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                type="button" data-ripple-light="true">
-                Signup With Google
-            </button>
-            <button onClick={handleGoogleSignOut}
-                className="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                type="button" data-ripple-light="true">
-                Sign Out
-            </button>
+    {/* user ? logout : signin */}
+    {
+        user ?
+        <button onClick={handleGoogleSignOut}
+         className="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+         type="button" data-ripple-light="true">
+         Sign Out
+     </button> :
+     <button onClick={handleGoogleSignIn}
+         className="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+         type="button" data-ripple-light="true">
+         Signup With Google
+     </button> 
+     
+    }
+           
+           
             
             <div>
                 {
